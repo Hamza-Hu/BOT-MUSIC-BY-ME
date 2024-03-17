aa057af78f347a9374c492f5590dd3b8cdb5a446
Hamza-Hu <121388013+Hamza-Hu@users.noreply.github.com>
2024-03-17 16:16:39 +0000

    Update filter.js

--- commands/filter.js
+++ commands/filter.js
@@ -54,8 +54,8 @@ module.exports = {
       .setColor('#01fe66')
       .setAuthor({
           name: 'Audio Filters ',
-          iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157534645311766558/2353-arrowrightglow.gif?ex=6518f5a5&is=6517a425&hm=ce55696f7ed85e2f7a97a3505eb39016fa9cd0c50be043efdf0cce06d7126b4c&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          iconURL: 'https://cdn.discordapp.com/attachments/1218730959197966447/1218953025737982012/standard.gif?ex=66098978&is=65f71478&hm=71f544a060803d8580aa74a215ab14aa4d45ad7e2b8da2963748894950e1eddd&',
+          url: 'https://discord.gg/gAqyGWeDym'
         })
       .setDescription('** Explore the Beat, Choose Your Sound Magic Below!**')
   
