8495c71e3bd96b2f6ae18e224c3e3f1b4663b9c9
Hamza-Hu <121388013+Hamza-Hu@users.noreply.github.com>
2024-04-10 20:09:00 +0000

    Update playSong.js

--- events/player/playSong.js
+++ events/player/playSong.js
@@ -9,7 +9,7 @@ module.exports = async (client, queue, song) => {
       .setAuthor({
         name: 'Currently playing a Track',
         iconURL: 'https://cdn.discordapp.com/attachments/1218730959197966447/1218953025737982012/standard.gif?ex=66098978&is=65f71478&hm=71f544a060803d8580aa74a215ab14aa4d45ad7e2b8da2963748894950e1eddd&', 
-        url: 'https://discord.gg/gAqyGWeDym'
+        url: 'https://discord.gg/6kvJChM28m'
     })
     .setDescription(`\n ‎ \n▶️ **Details :** **${song?.name}**\n▶️ **Enjoy the Ultimate Music Experience. ** \n▶️ **If link breaks playback try to give query.**`)
 .setImage(queue.songs[0].thumbnail)
@@ -20,19 +20,3 @@ module.exports = async (client, queue, song) => {
     }
   }
 }
-
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
