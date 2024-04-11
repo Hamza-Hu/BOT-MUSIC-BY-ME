3dba85b6ce7bb83e81b7b1dcceb7794f41257a72
Hamza-Hu <121388013+Hamza-Hu@users.noreply.github.com>
2024-04-11 02:49:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,27 +7,6 @@ const manager = new ShardingManager('./bot.js', { token: config.TOKEN || process
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
 
-client.on('ready', () => {
-
-  console.log(`Logged in as ${client.user.tag}!`);
-
-  // Set bot status
-
-  client.user.setPresence({
-
-    activities: [{
-
-      name: `MrakTeam Dev`,
-        name : `By Mrakchi /Help` ,
-      type: 'WATCHING'
-
-    }],
-
-    status: 'idle'
-
-  });
-
-});
 } else {
 
 require("./bot.js")
