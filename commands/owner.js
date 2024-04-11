e7ebc220d60f0f046e3bb4b7c074f892e009a28c
Hamza-Hu <121388013+Hamza-Hu@users.noreply.github.com>
2024-04-11 02:58:28 +0000

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,18 +1,3 @@
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
 const { ApplicationCommandOptionType } = require('discord.js');
 const db = require("../mongoDB");
 
@@ -25,7 +10,7 @@ module.exports = {
   run: async (client, interaction) => {
     try {
       const youtubeLink = 'https://youtube.com/@mrakchi26?si=hNFm-hMCF6x8fbxO';
-      const InstagramLink = 'https://discord.gg/YkFWvD2AKd';
+      const InstagramLink = 'https://www.instagram.com/mrakchi_5?igsh=OGQ5ZDc2ODk2ZA==';
       const { EmbedBuilder } = require('discord.js')
         const embed = new EmbedBuilder()
             .setColor('#da2a41')
@@ -34,7 +19,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1219371335592050759/1223409877985267782/d562d4205927c8d1ca5eed0adcaaa25d.jpg?ex=6619c03c&is=66074b3c&hm=7c0fdae3e30548efa2b1b594062f202a036f07c1dfacb80bca13023cfac1ff85&',
           url: 'https://discord.gg/6kvJChM28m'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ MRAKCHI. I am a discord bot developer. I love playing games. my Team : TEAM LHAJ. Feel free to contact me!\n YouTube : ❤️ [MRAKCHI](${youtubeLink})\n Discord : 💙 [ASTA COMMUNITY](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n\n ▶️ MRAKCHI. I am a discord bot developer. I love playing games. my Team : WickStudio | MrakTeam. Feel free to contact me!\n YouTube : ❤️ [Click here](${youtubeLink})\n Instagram : 💙 [Click here](${InstagramLink})`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
@@ -43,18 +28,3 @@ module.exports = {
   }
   },
 };
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
